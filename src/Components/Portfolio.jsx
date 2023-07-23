e22b9dbbd767d17d2b9951e49c38a72e835d154e
Shifraz Nazar <109841959+ShifrazNazar@users.noreply.github.com>
2023-07-23 14:39:09 +0000

    Add Portfolio Information

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/pattern.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "pattern";
 
 /**
  * Project list
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Linkedin Clone",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "I have developed a LinkedIn clone website utilizing ReactJS, Redux, and Firebase by following a tutorial led by Sonny Sangha. The platform allows users to establish login profiles and features a live chat system. This project serves as a demonstration of my proficiency in web development, showcasing my capability to create functional, user-friendly websites.",
+    url: "https://linkedin-clone-6b0d4.web.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Portfolio Website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "The website being viewed was created by me. Its development was motivated by my desire to gain more knowledge about Next JS, Typescript, Framer Motion, Tailwind and Sanity CMS. This website incorporates a backend using Sanity, which allows for updates to be made to the website's information without requiring the rebuilding of the application. The initial inspiration for the website was derived from a video on YouTube presented by Sonny Sangha.",
+    url: "https://portfolio-ruby-eta-31.vercel.app/",
   },
   {
-    title: "My Resume Site",
+    title: "Weather App",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "The weather app was created using HTML, CSS, JavaScript, and RapidAPI. The app allows users to search for a location and displays the current weather information for that location, including temperature, sun rise and sun set. The app has a clean and user-friendly interface and is responsive on any device. The project demonstrates proficiency in HTML, CSS, JavaScript, and working with APIs.",
+    url: "https://shifraznazar.github.io/Weather-API/",
   },
 ];
 
